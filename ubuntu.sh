dfa3e43884880d334b2fbe97a1ce1d4da96a1450
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-10 00:01:25 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,6 +3,8 @@
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 echo "This is a script to help you to have a macos vm with sosumi"
 echo "The script will now install snap. If you have already it,it will do nothing"
+sudo apt update
+sudo apt install snapd
 sudo systemctl enable --now snapd.socket
 sudo snap install sosumi
 sudo snap connect sosumi:kvm
