11d8db35782c31e1d9cf3b13f3d77a68af4ae553
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-14 02:07:04 +0200

    Update raspberry-pi-os.sh

--- raspberry-pi-os.sh
+++ raspberry-pi-os.sh
@@ -9,6 +9,8 @@ sudo snap install core
 sudo systemctl enable --now snapd.socket
 sudo snap install sosumi
 sudo snap connect sosumi:kvm
+echo "You will now need to close the window that the script open'
+
 sosumi
  
 cd ~/snap/sosumi/common/
